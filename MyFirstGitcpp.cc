76a31726499c7b47b261a39a6d2272e86a09cf18
MdGhouze <mdghouse.1917@gmail.com>
2018-09-29 21:56:31 +0530

    MyFirstCppCommit

---
+++ MyFirstGitcpp.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, const char *argv)
+{
+		std::cout<<"My First Cpp Commint"<<std::endl;
+		std::cout<<"Hurrey Commit is successfull"<<std::endl;
+		return 0;	
+}
